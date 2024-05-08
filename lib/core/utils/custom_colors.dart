ade0ef3e4bf17c82cc583f5d9ffccbf1d62df70b
Ayaabq <ayabakleh2004@gmail.com>
2024-05-08 16:16:06 +0300

    Add custom colors, styles, and plugin registration
    Added custom colors and styles classes for consistent theming. Also registered
    the PathProviderPlugin in the macOS GeneratedPluginRegistrant file. Updated
    dependencies for bloc, cupertino_icons, and added google_fonts.

---
+++ lib/core/utils/custom_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class CustomColors {
+  static List<Color> violet = const [
+    Color(0xff7317eb),
+  ];
+}
---
+++ lib/core/utils/styles.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+class Styles {
+  static const textStyle18 = TextStyle();
+}
