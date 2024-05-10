175208d67fcc6fa7024b91c990713a3a03d60f47
Ayaabq <ayabakleh2004@gmail.com>
2024-05-10 22:19:22 +0300

    add on boarding assets

--- lib/core/utils/assets.dart
+++ lib/core/utils/assets.dart
@@ -4,9 +4,9 @@ class AssetsData {
 }
 
 class OnBoardingAssets extends AssetsData {
-  static const onBoaring1 = 'assets\images\on_boarding\on_boarding_1.svg';
-  static const onBoaring2 = 'assets\images\on_boarding\on_boarding_2.svg';
-  static const onBoaring3 = 'assets\images\on_boarding\on_boarding_3.svg';
-  static const onBoaring4 = 'assets\images\on_boarding\on_boarding_4.svg';
-  static const onBoaring5 = 'assets\images\on_boarding\on_boarding_5.svg';
+  static const onBoaring1 = 'assets/images/on_boarding/on_boarding_1.svg';
+  static const onBoaring2 = 'assets/images/on_boarding/on_boarding_2.svg';
+  static const onBoaring3 = 'assets/images/on_boarding/on_boarding_3.svg';
+  static const onBoaring4 = 'assets/images/on_boarding/on_boarding_4.svg';
+  static const onBoaring5 = 'assets/images/on_boarding/on_boarding_5.svg';
 }
