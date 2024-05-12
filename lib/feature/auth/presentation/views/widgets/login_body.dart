93b390a91e181f61fd967e0d60a0d32cd9d444ff
Ayama11 <ayamalla882@gmail.com>
2024-05-12 21:50:49 +0300

    Add SafeArea to LoginView and create HeaderVector widget
    The commit adds SafeArea to LoginView for better UI handling and introduces
    the HeaderVector widget to display header images in the authentication screen.

--- lib/feature/auth/presentation/views/widgets/login_body.dart
+++ lib/feature/auth/presentation/views/widgets/login_body.dart
@@ -5,6 +5,10 @@ class LoginBodyView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const SingleChildScrollView(
+      child: Column(
+        children: [],
+      ),
+    );
   }
 }
