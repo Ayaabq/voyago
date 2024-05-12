12da0b45b87b0ed579f1062ac47ca74eab8d7ddf
Ayama11 <ayamalla882@gmail.com>
2024-05-13 00:26:25 +0300

    Add custom text field widget and refactor login body view
    Added a custom text field widget for reusability and refactored the login body view
    to use the new custom text field widget for email and password fields.

---
+++ lib/feature/auth/presentation/views/widgets/custom_text_field.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+
+class TextFieldCustom extends StatelessWidget {
+  const TextFieldCustom(
+      {super.key,
+      required this.hint,
+      required this.onPressedIcon,
+      required this.icon});
+
+  final String hint;
+  final void Function() onPressedIcon;
+  final Widget icon;
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      decoration: InputDecoration(
+        labelText: hint,
+        suffixIcon: IconButton(onPressed: onPressedIcon, icon: icon),
+        enabledBorder: buildBorder(),
+        focusedBorder: buildBorder(const Color(0xff8879CF)),
+      ),
+    );
+  }
+
+  OutlineInputBorder buildBorder([color]) {
+    return OutlineInputBorder(
+        borderRadius: BorderRadius.circular(14),
+        borderSide:
+            BorderSide(color: color ?? const Color(0xffC4C4C4), width: 1));
+  }
+}
--- lib/feature/auth/presentation/views/widgets/login_body.dart
+++ lib/feature/auth/presentation/views/widgets/login_body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:voyago/core/utils/styles.dart';
+import 'package:voyago/feature/auth/presentation/views/widgets/custom_text_field.dart';
 import 'package:voyago/feature/auth/presentation/views/widgets/header_auth.dart';
 import 'package:iconsax/iconsax.dart';
 
@@ -24,20 +25,56 @@ class LoginBodyView extends StatelessWidget {
                   style: Styles.textStyle25W700,
                 ),
                 const SizedBox(height: 32),
+                const FormLogin(),
+                const SizedBox(height: 20),
+                Padding(
+                  padding: const EdgeInsets.all(0),
+                  child: Row(
+                    children: [
+                      TextButton(
+                          onPressed: () {},
+                          child: Text(
+                            "Forgot your password?",
+                            style: Styles.textStyle16W700
+                                .copyWith(color: const Color(0xff5E4DA7)),
+                          ))
+                    ],
+                  ),
+                )
               ],
             ),
-          ),
-          Form(
-              child: Column(
-            children: [
-              TextFormField(
-                decoration:
-                    InputDecoration(suffixIcon: Icon(Iconsax.direct_inbox)),
-              )
-            ],
-          ))
+          )
         ],
       ),
     );
   }
 }
+
+class FormLogin extends StatelessWidget {
+  const FormLogin({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+        child: Column(
+      children: [
+        TextFieldCustom(
+          hint: "Email",
+          icon: const Icon(Iconsax.add),
+          onPressedIcon: () {},
+        ),
+        const SizedBox(height: 28),
+        TextFieldCustom(
+          hint: "Password",
+          icon: const Icon(Iconsax.password_check),
+          onPressedIcon: () {},
+        )
+      ],
+    ));
+  }
+}
+// login 700 20
+//Don't  have an account? 400 16
+//Create Now 700 16
