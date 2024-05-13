862795e8524cbe5949ee932c816cef3016673b61
Ayama11 <ayamalla882@gmail.com>
2024-05-13 09:24:38 +0300

    Remove unused imports and update iconsax dependency
    - Remove unused imports in custom_text_field.dart and get_started.dart
    - Update iconsax dependency in pubspec.yaml and pubspec.lock to "direct main" instead of "direct dev"

--- lib/feature/auth/presentation/views/widgets/custom_text_field.dart
+++ lib/feature/auth/presentation/views/widgets/custom_text_field.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:iconsax/iconsax.dart';
 
 class TextFieldCustom extends StatelessWidget {
   const TextFieldCustom(
--- lib/feature/get_started/presentation/views/get_started.dart
+++ lib/feature/get_started/presentation/views/get_started.dart
@@ -1,18 +1,14 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import 'package:voyago/feature/get_started/presentation/views/widgets/get_started_body.dart';
-
 
 class GetStarted extends StatelessWidget {
   const GetStarted({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  const  Scaffold(
-     body: GetStartedBody(),
-
+    return const Scaffold(
+      body: GetStartedBody(),
     );
   }
 }
-
