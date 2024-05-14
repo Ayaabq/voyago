cf019be55f5a581230da65df6cf1cb9d91308db3
Ayaabq <ayabakleh2004@gmail.com>
2024-05-14 13:22:41 +0300

    adding dummy discount

--- lib/generated/assets.dart
+++ lib/generated/assets.dart
@@ -5,7 +5,7 @@ class Assets {
   static const String homeBackgrounSky = 'assets/images/home/backgroun_sky.png';
   static const String homeCategoryAttractionsIcon = 'assets/images/home/category_attractions_icon.png';
   static const String homeCategoryTripsIcon = 'assets/images/home/category_trips_icon.png';
-  static const String homeImg = 'assets/images/home/img.png';
+  static const String homeImg = 'assets/images/home/dummy_discount.png';
   static const String imagesOnBoardingOnBoarding5 = 'assets/images/on_boarding/on_boarding_5.dart';
   static const String loginVector1 = 'assets/images/login/Vector1.png';
   static const String loginVector2 = 'assets/images/login/Vector2.png';
