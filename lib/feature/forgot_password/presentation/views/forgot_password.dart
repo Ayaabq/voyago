8f8612dfe230a8d33b037b8cb50945f7a2b00f44
Ayama11 <ayamalla882@gmail.com>
2024-05-14 11:32:51 +0300

    Update ForgotPasswordView body to use ForgotPasswordBody widget
    Changed the body of ForgotPasswordView to use the ForgotPasswordBody widget
    for better organization and clarity in the code.

--- lib/feature/forgot_password/presentation/views/forgot_password.dart
+++ lib/feature/forgot_password/presentation/views/forgot_password.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:voyago/feature/forgot_password/presentation/views/widgets/forgot_password_body.dart';
 
 class ForgotPasswordView extends StatelessWidget {
   const ForgotPasswordView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: ForgotPasswordView());
+    return const Scaffold(body: ForgotPasswordBody());
   }
 }
