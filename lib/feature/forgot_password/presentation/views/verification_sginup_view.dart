dd808c2856fd90efd4a5ca5fa361be436f440d81
Ayama11 <ayamalla882@gmail.com>
2024-05-15 16:07:34 +0300

    Add new verification code and signup views and related widgets
    Added new views for verification code and signup along with their respective
    widgets to handle code verification and signup process in the forgot password
    feature.

---
+++ lib/feature/forgot_password/presentation/views/verification_sginup_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class VerificationSginUpView extends StatelessWidget {
+  const VerificationSginUpView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
