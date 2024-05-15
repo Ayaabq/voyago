eb398ee9b677c750f23a3c009cb8cf60719931d3
Ayama11 <ayamalla882@gmail.com>
2024-05-15 14:23:25 +0300

    Update text field components and form layouts
    Add new keyboard types and validators to text field components.
    Update form layouts in form_login and form_register widgets.

--- lib/feature/auth/presentation/views/widgets/custom_text_field.dart
+++ lib/feature/auth/presentation/views/widgets/custom_text_field.dart
@@ -1,116 +1,6 @@
-// import 'package:flutter/material.dart';
-// import 'package:flutter_hooks/flutter_hooks.dart';
-// import 'package:voyago/core/utils/custom_colors.dart';
-
-// class TextFieldCustom extends HookWidget {
-//   const TextFieldCustom(
-//       {super.key,
-//       required this.hint,
-//       required this.onPressedIcon,
-//       required this.icon});
-
-//   final String hint;
-//   final void Function() onPressedIcon;
-//   final Icon icon;
-//   @override
-//   Widget build(BuildContext context) {
-//     final focusNode = useFocusNode();
-//     return TextFormField(
-//       focusNode: focusNode,
-//       decoration: InputDecoration(
-//         labelText: hint,
-//      //   hintText: hint,
-//         labelStyle: const TextStyle(
-//           fontSize: 12,
-//         ),
-//         suffixIcon: IconButton(
-//             onPressed: onPressedIcon,
-//             icon: icon,
-//             color: focusNode.hasFocus
-//                 ? CustomColors.kMove[4]
-//                 : CustomColors.kGrey[0]),
-//         enabledBorder: buildBorder(),
-//         focusedBorder: buildBorder(CustomColors.kMove[4]),
-//       ),
-//     );
-//   }
-
-//   OutlineInputBorder buildBorder([color]) {
-//     return OutlineInputBorder(
-//         borderRadius: BorderRadius.circular(14),
-//         borderSide:
-//             BorderSide(color: color ?? CustomColors.kGrey[0], width: 1));
-//   }
-// }
-// import 'package:flutter/material.dart';
-// import 'package:voyago/core/utils/custom_colors.dart';
-
-// class TextFieldCustom extends StatefulWidget {
-//   const TextFieldCustom(
-//       {super.key,
-//       required this.hint,
-//       required this.onPressedIcon,
-//       required this.icon});
-
-//   final String hint;
-//   final void Function() onPressedIcon;
-//   final Icon icon;
-
-//   @override
-//   _TextFieldCustomState createState() => _TextFieldCustomState();
-// }
-
-// class _TextFieldCustomState extends State<TextFieldCustom> {
-//   final FocusNode _focusNode = FocusNode();
-
-//   @override
-//   void initState() {
-//     super.initState();
-//     _focusNode.addListener(_onFocusChange);
-//   }
-
-//   @override
-//   void dispose() {
-//     _focusNode.removeListener(_onFocusChange);
-//     _focusNode.dispose();
-//     super.dispose();
-//   }
-
-//   void _onFocusChange() {
-//     setState(() {});
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return TextFormField(
-//       focusNode: _focusNode,
-//       decoration: InputDecoration(
-//         labelText: widget.hint,
-//         labelStyle: const TextStyle(
-//           fontSize: 12,
-//         ),
-//         suffixIcon: IconButton(
-//           onPressed: widget.onPressedIcon,
-//           icon: widget.icon,
-//           color: _focusNode.hasFocus
-//               ? CustomColors.kMove[4]
-//               : CustomColors.kGrey[0],
-//         ),
-//         enabledBorder: buildBorder(),
-//         focusedBorder: buildBorder(CustomColors.kMove[4]),
-//       ),
-//     );
-//   }
-
-//   OutlineInputBorder buildBorder([color]) {
-//     return OutlineInputBorder(
-//       borderRadius: BorderRadius.circular(14),
-//       borderSide: BorderSide(color: color ?? CustomColors.kGrey[0], width: 1),
-//     );
-//   }
-// }
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:regexpattern/regexpattern.dart';
 import 'package:voyago/core/utils/custom_colors.dart';
 
 class TextFieldCustom extends HookWidget {
@@ -120,13 +10,14 @@ class TextFieldCustom extends HookWidget {
     required this.onPressedIcon,
     required this.icon,
     required this.keyboardType,
+    // required this.validator,
   });
 
   final String hint;
   final void Function() onPressedIcon;
   final Icon icon;
   final TextInputType keyboardType;
-
+  // final String Function(String?) validator;
   @override
   Widget build(BuildContext context) {
     final focusNode = useFocusNode();
@@ -134,7 +25,8 @@ class TextFieldCustom extends HookWidget {
     return SizedBox(
       height: 50,
       child: TextFormField(
-        keyboardType: TextInputType.number,
+        //    validator: validator,
+        keyboardType: keyboardType,
         focusNode: focusNode,
         decoration: InputDecoration(
           labelText: hint,
