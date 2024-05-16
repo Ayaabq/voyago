36dfbe89b01a9b730d62d64c3c8cdb839ed9641f
Ayaabq <ayabakleh2004@gmail.com>
2024-05-17 00:42:40 +0300

    never know

--- lib/feature/home/presentation/views/home_view.dart
+++ lib/feature/home/presentation/views/home_view.dart
@@ -1,12 +1,29 @@
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
 import 'package:voyago/feature/home/presentation/views/widgets/home_view_body.dart';
+
+import '../../../../core/utils/custom_colors.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return  Scaffold(
+      bottomNavigationBar: CurvedNavigationBar(
+        backgroundColor: Colors.transparent,
+        buttonBackgroundColor: CustomColors.kMove[0],
+        color: CustomColors.kMove[0],
+        animationDuration: const Duration(microseconds: 300),
+        items: const [
+          Icon(Iconsax.home),
+          Icon(Iconsax.bag_2),
+          Icon(Iconsax.heart),
+          Icon(Iconsax.profile_add),
+        ],
+
+      ) ,
       body: HomeViewBody(),
     );
   }
