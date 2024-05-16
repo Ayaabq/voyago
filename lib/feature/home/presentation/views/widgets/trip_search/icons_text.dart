768ba1417bffe9acbde194fda1327d01b3599375
Ayama11 <ayamalla882@gmail.com>
2024-05-17 01:04:09 +0300

    Add trip search card components
    Added new components for trip search card including IconsText, TimeTrip,
    and TripSearchCard to display trip information in the UI.

---
+++ lib/feature/home/presentation/views/widgets/trip_search/icons_text.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:voyago/core/utils/custom_colors.dart';
+import 'package:voyago/core/utils/styles.dart';
+
+class IconsText extends StatelessWidget {
+  const IconsText(
+      {super.key,
+      required this.text,
+      required this.icon,
+      required this.colorIcon,
+      required this.sizeIcon, this.style});
+  final String text;
+  final IconData icon;
+  final Color colorIcon;
+  final double sizeIcon;
+  final TextStyle? style;
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Icon(
+          icon,
+          size: sizeIcon,
+          color: colorIcon,
+        ),
+        // Icon(
+        //   Icons.location_on,
+        //   color: CustomColors.kGrey[2],
+        //   size: 10,
+        // ),
+        const SizedBox(width: 2),
+        Text(
+          text,
+          style: Styles.textStyle10W400,
+        ),
+      ],
+    );
+  }
+}
