4e077a6e01bbbf273cfa87103983d872892d612b
Ayaabq <ayabakleh2004@gmail.com>
2024-05-17 00:21:32 +0300

    adding bottom nav bar

--- lib/main.dart
+++ lib/main.dart
@@ -22,10 +22,12 @@ class VoyagoApp extends StatelessWidget {
   Widget build(BuildContext context) {
     ScreenSizeUtil.init(context);
     return MaterialApp.router(
+
       debugShowCheckedModeBanner: false,
+
       routerConfig: AppRouter.router,
       theme: ThemeData.light().copyWith(
-          textTheme:
+        textTheme:
               GoogleFonts.montserratTextTheme(ThemeData.light().textTheme),),
 
       // home: const GetStarted(),
