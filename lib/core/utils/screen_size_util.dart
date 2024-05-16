e8cce9d2cd32c0a871c5b884738763c1f303fd22
Ayaabq <ayabakleh2004@gmail.com>
2024-05-16 19:38:44 +0300

    making catygories responsiv

--- lib/core/utils/screen_size_util.dart
+++ lib/core/utils/screen_size_util.dart
@@ -8,4 +8,12 @@ class ScreenSizeUtil {
     screenWidth = MediaQuery.of(context).size.width;
     screenHeight = MediaQuery.of(context).size.height;
   }
+
+  static double dynamicWidth(double value) {
+    return screenWidth * value;
+  }
+
+  static double dynamicHeight(double value) {
+    return screenHeight * value;
+  }
 }
