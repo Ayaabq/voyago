49fb69a853a3e2a970c616ee797e1d767970d285
Ayaabq <ayabakleh2004@gmail.com>
2024-07-06 12:58:56 +0300

    adding passowrd to chekout stap 3 as ui

--- lib/feature/trip&booking/presentation/views/widgets/checkout/step3_payment_page.dart
+++ lib/feature/trip&booking/presentation/views/widgets/checkout/step3_payment_page.dart
@@ -1,7 +1,9 @@
 
 import 'package:flutter/material.dart';
 import 'package:voyago/core/utils/styles.dart';
+import 'package:voyago/core/utils/validator_manager.dart';
 import 'package:voyago/core/widgets/custom_card.dart';
+import 'package:voyago/feature/auth/login/presentation/views/widgets/custom_text_field.dart';
 import 'package:voyago/feature/trip&booking/presentation/views/widgets/checkout/price_details_section.dart';
 
 class Step3PaymentPage extends StatelessWidget {
@@ -9,15 +11,24 @@ class Step3PaymentPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return   const CustomCard(
+    return    CustomCard(
       title: "Payment Details",
-      content: Column(
-        children: [
-          Text("Enter your password to verify the payment process.",
-          style: Styles.textStyle13W400,),
-
-          PriceDetailsSection(),
-        ],
+      content: Expanded(
+        child: ListView(
+          children: [
+            const Text("Enter your password to verify the payment process.",
+            style: Styles.textStyle13W400,),
+            const SizedBox(height: 20,),
+            TextFieldCustom(hint: "Password",
+                onPressedIcon: (){},
+                icon: const Icon(Icons.remove_red_eye), keyboardType: TextInputType.visiblePassword,
+                validator: (v)=>ValidatorManager().validatePassword(v)),
+        
+        
+            const SizedBox(height: 20,),
+            const PriceDetailsSection(),
+          ],
+        ),
       ),
     );
   }
